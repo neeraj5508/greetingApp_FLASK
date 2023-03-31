c289c6f8fb17041c4f6e788d2d46ad2ec322d839
neeraj5508 <128129400+neeraj5508@users.noreply.github.com>
2023-03-31 17:40:52 +0545

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "manbearpig_MUDMAN888"
 
+
 @app.route("/hello")
 def index():
 	flash("what's your name?")
 	return render_template("index.html")
+
 
 @app.route("/greet", methods=['POST', 'GET'])
 def greeter():
